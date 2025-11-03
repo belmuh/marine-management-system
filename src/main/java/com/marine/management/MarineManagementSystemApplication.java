0a7d12d2de9f129e59b9713503f9251b12e97c30
belmaaz <belmaa@gmail.com>
2025-11-03 23:18:25 +0300

    Initial commit for marine management system

---
+++ src/main/java/com/marine/management/MarineManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.marine.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarineManagementSystemApplication.class, args);
+	}
+
+}
